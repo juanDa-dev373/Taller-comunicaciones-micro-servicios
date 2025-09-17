b6f02b574cc4c609585651bc9769a7ec20bb7a8d
Juanda-dev2003 <juanda.fonseca2003@gmail.com>
2025-09-16 19:25:49 -0500

    first commit

---
+++ src/main/java/org/project/sura/tallercomunicacionesmicroservicios/TallerComunicacionesMicroServiciosApplication.java
@@ -0,0 +1,13 @@
+package org.project.sura.tallercomunicacionesmicroservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerComunicacionesMicroServiciosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallerComunicacionesMicroServiciosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/sura/tallercomunicacionesmicroservicios/TallerComunicacionesMicroServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.sura.tallercomunicacionesmicroservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerComunicacionesMicroServiciosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
